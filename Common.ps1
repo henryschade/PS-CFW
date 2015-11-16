47fd598c99f8e1dc4cf1a14c17ddf6ac0a6f9861
henryschade <hschade@trinisoft.com>
2015-11-16 16:48:06 -0700

    Start GetFolderPathing() routine.

--- Common.ps1
+++ Common.ps1
@@ -9,8 +9,105 @@
 	#$ScriptDir = Split-Path $MyInvocation.MyCommand.Path;
 	#. ($ScriptDir + "\Common.ps1")
 
-	#Place holder function for legacy code.
+	function CleanDir{
+		#Cleans files out of directories based on the DateLastModified.  
+		#Checks the "NumDays2KeepLogs" entry in MiscSettings.txt file, if $HowOld is -2, blank, or null.
+		#   (180 days) if error reading NumDays2KeepLogs.
+		Param(
+			[ValidateNotNull()][Parameter(Mandatory=$True)][String]$Directory, 
+			[ValidateNotNull()][Parameter(Mandatory=$False)][Bool]$DoSubs = $False, 
+			[ValidateNotNull()][Parameter(Mandatory=$False)][String]$TypesToSkip = "", 
+			[ValidateNotNull()][Parameter(Mandatory=$False)][Int]$HowOld = -2
+		)
+		#$Directory = Folder/Directory path to clean.  i.e. "C:\SRM_Apps_N_Tools" or "\\Nawesdnifs08.nadsuswe.nads.navy.mil\NMCIISF\NMCIISF-SDCP-MAC\MAC\Boise SRM Team\Jay_Nielson\Reports"
+		#$DoSubs = True/False. (defult = False) Check/Clean sub folders too.
+		#$TypesToSkip = file types NOT to delete/clean. 
+		#	i.e. ".mdb" or ".ps1" or ".zip"
+		#	Supports "!" (not) (as the first char).  i.e. "!.tmp" (it will only delete these file types).
+		#		I want to make this support a ; seperated list of file types too.   i.e. ".mdb; .zip; .xlsx"
+		#$HowOld = How many days old does the file need to be, to be deleted.
+
+		$strSettingFile = "\\nawesdnifs08.nadsuswe.nads.navy.mil\NMCIISF\NMCIISF-SDCP-MAC\MAC\Entr_SRM\Support Files\MiscSettings.txt";
+
+		if (($HowOld -lt -1) -or ($HowOld -eq "") -or ($HowOld -eq $null)){
+			if ((Test-Path $strSettingFile)){
+				$Error.Clear();
+				foreach ($strLine in [System.IO.File]::ReadAllLines("\\nawesdnifs08.nadsuswe.nads.navy.mil\NMCIISF\NMCIISF-SDCP-MAC\MAC\Entr_SRM\Support Files\MiscSettings.txt")) {
+					if ($strLine.StartsWith("--") -ne $True){
+						if ($strLine.Contains("NumDays2KeepLogs")){
+							$HowOld = $strLine.SubString($strLine.IndexOf("=") + 1).Trim();
+							break;
+						}
+					}
+				}
+			}
+
+			##http://stackoverflow.com/questions/10928030/in-powershell-how-can-i-test-if-a-variable-holds-a-numeric-value
+			#Add-Type -Assembly Microsoft.VisualBasic;
+			#if (!([Microsoft.VisualBasic.Information]::IsNumeric($HowOld))){
+			#	$HowOld = 180;
+			#}
+			#I have since wrote a PS equivelent of the VB IsNumeric() routine.  (Part of Common.ps1)
+			if ((IsNumeric $HowOld) -ne $True){
+				$HowOld = 180;
+			}
+		}
+
+		if (Test-Path $Directory){
+			$objDirectory = Get-ChildItem -Path $Directory -ErrorAction SilentlyContinue -Force;			#force is necessary to get hidden files/folders;
+			foreach ($objItem in $objDirectory){
+				if ($objItem -ne $null){
+					$objSubItem = Get-Item -LiteralPath $objItem.Fullname -Force -ErrorAction Stop;			#force is necessary to get hidden files/folders
+					if ($objSubItem.PSIsContainer){
+						#Directory
+						if ($DoSubs -eq $True){
+							CleanDir $objSubItem.Fullname $DoSubs $TypesToSkip $HowOld;
+
+							$Error.Clear();
+							$objTest = Get-ChildItem -Path $objSubItem.Fullname -ErrorAction SilentlyContinue -Force;			#force is necessary to get hidden files/folders;
+							if (($objTest -eq $null) -and (!($Error))){
+								Remove-Item $objSubItem.Fullname -Force;
+							}
+						}
+					}else{
+						#File
+						#Write-Host $objSubItem.Fullname;
+						#Write-Host $objSubItem.LastWriteTime;
+						#Write-Host $objSubItem.CreationTime;
+						#Write-Host [IO.File]::GetLastWriteTime($objItem.Fullname);
+						$dteNow = [System.DateTime]::Now;
+						#if (($objSubItem.LastWriteTime -lt ($dteNow.AddDays([Int]($HowOld * -1)))) -and ($objSubItem.CreationTime -lt ($dteNow.AddDays(-1)))){
+						if ($objSubItem.LastWriteTime -lt ($dteNow.AddDays([Int]($HowOld * -1)))){
+							if ($TypesToSkip -eq ""){
+								#Delete it
+								Remove-Item $objSubItem.Fullname -Force;
+							}else{
+								#See if the file is one to NOT delete.
+								if ($TypesToSkip.Contains(";")){
+									#multiple file types specified.  Still coding this feature.
+								}else{
+									if ($TypesToSkip.StartsWith("!")){
+										if ($objSubItem.Name.EndsWith($TypesToSkip.SubString(1))){
+											#Delete it
+											Remove-Item $objSubItem.Fullname -Force;
+										}
+									}else{
+										if ($objSubItem.Name.EndsWith($TypesToSkip) -eq $False){
+											#Delete it
+											Remove-Item $objSubItem.Fullname -Force;
+										}
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
 	function CreateZipFile{
+		#Should use ZipCreateFile() in Documents.ps1.
 		Param(
 			[ValidateNotNull()][Parameter(Mandatory=$True, HelpMessage = "Zip file path to create.")][String]$ZipFile, 
 			[ValidateNotNull()][Parameter(Mandatory=$True, HelpMessage = "Array of file path (full) to add.")][Array]$Files
@@ -29,6 +126,105 @@
 		$objReturn = ZipCreateFile $ZipFile $Files;
 
 		return $objReturn;
+	}
+
+	function EnableDotNet4{
+		#Checks if .NET 4 is enabled, and if NOT then creates the *.xml config file to enable .NET4 support.
+		Param(
+			[ValidateNotNull()][Parameter(Mandatory=$False)][Bool]$bISE2 = $False
+		)
+		#$bISE2 = $True or $False.  Create the "*\powershell_ise.exe.config" files along with the "*\powershell.exe.config" files.
+
+		#Returns $True if created config files (at least tried to).
+		#Returns $False if Config Files did NOT have to be created (.NET 4.x already enabled).
+
+		$bReturn = $False;
+
+		if ($PSVersionTable.CLRVersion.Major -lt 4){
+			$bReturn = $True;
+
+			#http://tfl09.blogspot.com/2010/08/using-newer-versions-of-net-with.html
+			#http://w3facility.org/question/powershell-load-dll-got-error-add-type-could-not-load-file-or-assembly-webdriver-dll-or-one-of-its-dependencies-operation-is-not-supported/
+			#http://www.adminarsenal.com/admin-arsenal-blog/powershell-running-net-4-with-powershell-v2
+			#http://www.bonusbits.com/wiki/HowTo:Enable_.NET_4_Runtime_for_PowerShell_and_Other_Applications
+
+			$strXML = "<?xml version=`"1.0`"?>`r`n<configuration>`r`n";
+			$strXML = $strXML + "`t<startup useLegacyV2RuntimeActivationPolicy=`"true`">`r`n";
+			$strXML = $strXML + "`t`t<supportedRuntime version=`"v4.0.30319`"/>`r`n`t`t<supportedRuntime version=`"v2.0.50727`"/>`r`n";
+			$strXML = $strXML + "`t</startup>`r`n`t<runtime>`r`n";
+			$strXML = $strXML + "`t`t<loadFromRemoteSources enabled=`"true`"/>`r`n";
+			$strXML = $strXML + "`t</runtime>`r`n";
+			$strXML = $strXML + "</configuration>`r`n";
+
+			#Ideally we would use $pshome, but it is not always right, so doing both potential directories manually.
+			$arrConfigFiles = @("C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe.config", "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe.config");
+			if ($bISE2){
+				$arrConfigFiles += "C:\Windows\System32\WindowsPowerShell\v1.0\powershell_ise.exe.config";
+				$arrConfigFiles += "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell_ise.exe.config";
+			}
+			#The following may have issues creating/copying the files due to permissions.
+			foreach ($strConfigFile in $arrConfigFiles){
+				if (Test-Path -Path ($strConfigFile + ".bak")){
+					#Have a backup copy already
+					$strXML | Out-File ($strConfigFile);
+				}else{
+					#Don't have a backup file yet.
+					if (Test-Path -Path ($strConfigFile)){
+						#Copy the original config file to *.bak.
+						Copy-Item -Path ($strConfigFile) -Destination ($strConfigFile + ".bak");
+						if ((Test-Path -Path ($strConfigFile + ".bak"))){
+							#Update/overwrite the config file
+							$strXML | Out-File ($strConfigFile);
+						}
+					}else{
+						#Config file does not exist, so create both
+						$strXML | Out-File ($strConfigFile + ".bak");
+						$strXML | Out-File ($strConfigFile);
+					}
+				}
+			}
+		}
+
+		return $bReturn;
+	}
+
+	function GetFolderPathing{
+		Param(
+			[ValidateNotNull()][Parameter(Mandatory=$False)][String]$sName
+		)
+		#Returns a PowerShell object.
+			#$objReturn.Name		= Name of this process, with paramaters passed in.
+			#$objReturn.Results		= $True or $False.  Was a path found/gotten.
+			#$objReturn.Message		= "Success" or the error message.
+			#$objReturn.Returns		= A DataTable of Path(s).
+		#$sName = The name of the path(s) to get.
+
+		#Setup the PSObject to return.
+		#http://stackoverflow.com/questions/21559724/getting-all-named-parameters-from-powershell-including-empty-and-set-ones
+		$CommandName = $PSCmdlet.MyInvocation.InvocationName;
+		$ParameterList = (Get-Command -Name $CommandName).Parameters;
+		$strTemp = "";
+		foreach ($key in $ParameterList.keys){
+			$var = Get-Variable -Name $key -ErrorAction SilentlyContinue;
+			if($var){$strTemp += "[$($var.name) = $($var.value)] ";}
+		}
+		$strTemp = $CommandName + "(" + $strTemp.Trim() + ")";
+		$objReturn = New-Object PSObject -Property @{
+			Name = $strTemp
+			Results = $False
+			Message = "Error"
+			Returns = "";
+		}
+
+		#1) Look for a local config file.
+		#2) Read the desired path out of the file.
+		#3) If path not exist, query DB for path info.
+		#4) Update local config file.
+		#5) Return the path info.
+		
+		#Andrew wants to do step in this order instead:
+		# 3, 1, 2, 4, 5
+
 	}
 
 	function isADInstalled{
@@ -134,163 +330,6 @@
 			}
 		}
 		return $bInstalled;
-	}
-
-	function CleanDir{
-		#Cleans files out of directories based on the DateLastModified.  
-		#Checks the "NumDays2KeepLogs" entry in MiscSettings.txt file, if $HowOld is -2, blank, or null.
-		#   (180 days) if error reading NumDays2KeepLogs.
-		Param(
-			[ValidateNotNull()][Parameter(Mandatory=$True)][String]$Directory, 
-			[ValidateNotNull()][Parameter(Mandatory=$False)][Bool]$DoSubs = $False, 
-			[ValidateNotNull()][Parameter(Mandatory=$False)][String]$TypesToSkip = "", 
-			[ValidateNotNull()][Parameter(Mandatory=$False)][Int]$HowOld = -2
-		)
-		#$Directory = Folder/Directory path to clean.  i.e. "C:\SRM_Apps_N_Tools" or "\\Nawesdnifs08.nadsuswe.nads.navy.mil\NMCIISF\NMCIISF-SDCP-MAC\MAC\Boise SRM Team\Jay_Nielson\Reports"
-		#$DoSubs = True/False. (defult = False) Check/Clean sub folders too.
-		#$TypesToSkip = file types NOT to delete/clean. 
-		#	i.e. ".mdb" or ".ps1" or ".zip"
-		#	Supports "!" (not) (as the first char).  i.e. "!.tmp" (it will only delete these file types).
-		#		I want to make this support a ; seperated list of file types too.   i.e. ".mdb; .zip; .xlsx"
-		#$HowOld = How many days old does the file need to be, to be deleted.
-
-		$strSettingFile = "\\nawesdnifs08.nadsuswe.nads.navy.mil\NMCIISF\NMCIISF-SDCP-MAC\MAC\Entr_SRM\Support Files\MiscSettings.txt";
-
-		if (($HowOld -lt -1) -or ($HowOld -eq "") -or ($HowOld -eq $null)){
-			if ((Test-Path $strSettingFile)){
-				$Error.Clear();
-				foreach ($strLine in [System.IO.File]::ReadAllLines("\\nawesdnifs08.nadsuswe.nads.navy.mil\NMCIISF\NMCIISF-SDCP-MAC\MAC\Entr_SRM\Support Files\MiscSettings.txt")) {
-					if ($strLine.StartsWith("--") -ne $True){
-						if ($strLine.Contains("NumDays2KeepLogs")){
-							$HowOld = $strLine.SubString($strLine.IndexOf("=") + 1).Trim();
-							break;
-						}
-					}
-				}
-			}
-
-			##http://stackoverflow.com/questions/10928030/in-powershell-how-can-i-test-if-a-variable-holds-a-numeric-value
-			#Add-Type -Assembly Microsoft.VisualBasic;
-			#if (!([Microsoft.VisualBasic.Information]::IsNumeric($HowOld))){
-			#	$HowOld = 180;
-			#}
-			#I have since wrote a PS equivelent of the VB IsNumeric() routine.  (Part of Common.ps1)
-			if ((IsNumeric $HowOld) -ne $True){
-				$HowOld = 180;
-			}
-		}
-
-		if (Test-Path $Directory){
-			$objDirectory = Get-ChildItem -Path $Directory -ErrorAction SilentlyContinue -Force;			#force is necessary to get hidden files/folders;
-			foreach ($objItem in $objDirectory){
-				if ($objItem -ne $null){
-					$objSubItem = Get-Item -LiteralPath $objItem.Fullname -Force -ErrorAction Stop;			#force is necessary to get hidden files/folders
-					if ($objSubItem.PSIsContainer){
-						#Directory
-						if ($DoSubs -eq $True){
-							CleanDir $objSubItem.Fullname $DoSubs $TypesToSkip $HowOld;
-
-							$Error.Clear();
-							$objTest = Get-ChildItem -Path $objSubItem.Fullname -ErrorAction SilentlyContinue -Force;			#force is necessary to get hidden files/folders;
-							if (($objTest -eq $null) -and (!($Error))){
-								Remove-Item $objSubItem.Fullname -Force;
-							}
-						}
-					}else{
-						#File
-						#Write-Host $objSubItem.Fullname;
-						#Write-Host $objSubItem.LastWriteTime;
-						#Write-Host $objSubItem.CreationTime;
-						#Write-Host [IO.File]::GetLastWriteTime($objItem.Fullname);
-						$dteNow = [System.DateTime]::Now;
-						#if (($objSubItem.LastWriteTime -lt ($dteNow.AddDays([Int]($HowOld * -1)))) -and ($objSubItem.CreationTime -lt ($dteNow.AddDays(-1)))){
-						if ($objSubItem.LastWriteTime -lt ($dteNow.AddDays([Int]($HowOld * -1)))){
-							if ($TypesToSkip -eq ""){
-								#Delete it
-								Remove-Item $objSubItem.Fullname -Force;
-							}else{
-								#See if the file is one to NOT delete.
-								if ($TypesToSkip.Contains(";")){
-									#multiple file types specified.  Still coding this feature.
-								}else{
-									if ($TypesToSkip.StartsWith("!")){
-										if ($objSubItem.Name.EndsWith($TypesToSkip.SubString(1))){
-											#Delete it
-											Remove-Item $objSubItem.Fullname -Force;
-										}
-									}else{
-										if ($objSubItem.Name.EndsWith($TypesToSkip) -eq $False){
-											#Delete it
-											Remove-Item $objSubItem.Fullname -Force;
-										}
-									}
-								}
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
-	function EnableDotNet4{
-		#Checks if .NET 4 is enabled, and if NOT then creates the *.xml config file to enable .NET4 support.
-		Param(
-			[ValidateNotNull()][Parameter(Mandatory=$False)][Bool]$bISE2 = $False
-		)
-		#$bISE2 = $True or $False.  Create the "*\powershell_ise.exe.config" files along with the "*\powershell.exe.config" files.
-
-		#Returns $True if created config files (at least tried to).
-		#Returns $False if Config Files did NOT have to be created (.NET 4.x already enabled).
-
-		$bReturn = $False;
-
-		if ($PSVersionTable.CLRVersion.Major -lt 4){
-			$bReturn = $True;
-
-			#http://tfl09.blogspot.com/2010/08/using-newer-versions-of-net-with.html
-			#http://w3facility.org/question/powershell-load-dll-got-error-add-type-could-not-load-file-or-assembly-webdriver-dll-or-one-of-its-dependencies-operation-is-not-supported/
-			#http://www.adminarsenal.com/admin-arsenal-blog/powershell-running-net-4-with-powershell-v2
-			#http://www.bonusbits.com/wiki/HowTo:Enable_.NET_4_Runtime_for_PowerShell_and_Other_Applications
-
-			$strXML = "<?xml version=`"1.0`"?>`r`n<configuration>`r`n";
-			$strXML = $strXML + "`t<startup useLegacyV2RuntimeActivationPolicy=`"true`">`r`n";
-			$strXML = $strXML + "`t`t<supportedRuntime version=`"v4.0.30319`"/>`r`n`t`t<supportedRuntime version=`"v2.0.50727`"/>`r`n";
-			$strXML = $strXML + "`t</startup>`r`n`t<runtime>`r`n";
-			$strXML = $strXML + "`t`t<loadFromRemoteSources enabled=`"true`"/>`r`n";
-			$strXML = $strXML + "`t</runtime>`r`n";
-			$strXML = $strXML + "</configuration>`r`n";
-
-			#Ideally we would use $pshome, but it is not always right, so doing both potential directories manually.
-			$arrConfigFiles = @("C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe.config", "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell.exe.config");
-			if ($bISE2){
-				$arrConfigFiles += "C:\Windows\System32\WindowsPowerShell\v1.0\powershell_ise.exe.config";
-				$arrConfigFiles += "C:\Windows\SysWOW64\WindowsPowerShell\v1.0\powershell_ise.exe.config";
-			}
-			#The following may have issues creating/copying the files due to permissions.
-			foreach ($strConfigFile in $arrConfigFiles){
-				if (Test-Path -Path ($strConfigFile + ".bak")){
-					#Have a backup copy already
-					$strXML | Out-File ($strConfigFile);
-				}else{
-					#Don't have a backup file yet.
-					if (Test-Path -Path ($strConfigFile)){
-						#Copy the original config file to *.bak.
-						Copy-Item -Path ($strConfigFile) -Destination ($strConfigFile + ".bak");
-						if ((Test-Path -Path ($strConfigFile + ".bak"))){
-							#Update/overwrite the config file
-							$strXML | Out-File ($strConfigFile);
-						}
-					}else{
-						#Config file does not exist, so create both
-						$strXML | Out-File ($strConfigFile + ".bak");
-						$strXML | Out-File ($strConfigFile);
-					}
-				}
-			}
-		}
-
-		return $bReturn;
 	}
 
 	function isNumeric($intX){
