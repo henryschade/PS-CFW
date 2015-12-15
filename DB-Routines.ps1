6ff0244b352f92cc450e353c5b253c54d3665c11
henryschade <hschade@trinisoft.com>
2015-12-15 13:47:54 -0700

    Updated sample code for adding Binary files.

--- DB-Routines.ps1
+++ DB-Routines.ps1
@@ -87,36 +87,25 @@
 		$dteDateTime = ([System.DateTime]::Now).ToString();
 
 		#---=== option 3 ===---
-		$intSize = (Get-ChildItem $strFilePath).Length;
-		Write-Host "$intSize bytes  (" ($intSize/1024) " KB)";
-		Write-Host "$intSize bytes  (" ($intSize/1024) " KB)  (" (($intSize/1024)/1024) " MB)";
+		#$intSize = (Get-ChildItem $strFilePath).Length;
+		#Write-Host "$intSize bytes  (" ($intSize/1024) " KB)";
+		#Write-Host "$intSize bytes  (" ($intSize/1024) " KB)  (" (($intSize/1024)/1024) " MB)";
 
 
-		Write-Host ([System.DateTime]::Now).ToString();
+		#Write-Host ([System.DateTime]::Now).ToString();
 		$objFile = [System.IO.File]::OpenRead($strFilePath);
 		$strFileByteArr = New-Object System.Byte[] $objFile.Length;
 		$objFile.Read($strFileByteArr, 0, $objFile.Length);
 		$objFile.Close();
-		Write-Host ([System.DateTime]::Now).ToString();
-		#5 sec  -->  4250438 bytes  ( 4150.82  KB)  ( 4.05  MB)
-		#3 sec  -->  7268352 bytes  ( 7098  KB)  ( 6.93  MB)
-
-
 		#Write-Host ([System.DateTime]::Now).ToString();
-		#[byte[]]$strFileByteArr = Get-Content $strFilePath -Encoding byte
-		#Write-Host ([System.DateTime]::Now).ToString();
-		##8 sec  -->  468kb
-		##5 hrs  -->  4250438 bytes  ( 4150.82  KB)  ( 4.05  MB)
 
-
-		#$objFS = new-object System.IO.FileStream($strFilePath,[System.IO.FileMode]'Open',[System.IO.FileAccess]'Read')
-		#$buffer = new-object byte[] -ArgumentList $objFS.Length
-		#$objFS.Read($buffer, 0, $buffer.Length)
-		#$objFS.Close()
-
+			#$objFS = new-object System.IO.FileStream($strFilePath,[System.IO.FileMode]'Open',[System.IO.FileAccess]'Read')
+			#$buffer = new-object byte[] -ArgumentList $objFS.Length
+			#$objFS.Read($buffer, 0, $buffer.Length)
+			#$objFS.Close()
 
 		$strSQL = "INSERT INTO SourceFiles(SourceDesc_GUID, Date_Up, File_Binary) VALUES ('$strGUID', '$dteDateTime', @File_Binary)"
-		$strSQL = "UPDATE SourceFiles(Date_Up = '$dteDateTime', File_Binary = @File_Binary) WHERE (GUID = '$strGUID')"
+		$strSQL = "UPDATE SourceFiles SET Date_Up = '$dteDateTime', File_Binary = @File_Binary WHERE (GUID = '$strGUID')"
 
 		#QueryDB() basic format
 		$connection=new-object System.Data.SqlClient.SQLConnection
